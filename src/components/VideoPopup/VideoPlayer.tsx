9f3b5a68d25b4d01816172c6f16a37a611ed85c7
shanky1001 <sr1071995@gmail.com>
2023-09-09 19:55:07 +0530

    added video player popup

---
+++ src/components/VideoPopup/VideoPlayer.tsx
@@ -0,0 +1,40 @@
+import { HiXMark } from "react-icons/hi2";
+import { heroComponent } from "types";
+
+const VideoPlayer = ({ play, setPlay }: heroComponent) => {
+	var items:string[] = ["iFxUw5Cckdw", "C-WQcCqecHg", "KUY2MMRmLLM"];
+	var item:string = items[Math.floor(Math.random() * items.length)];
+	return (
+		<div
+			className={` w-full h-screen
+       bg-black absolute  frc justify-center z-[99999] top-0  text-white  left-0`}
+		>
+			<div
+				onClick={() => setPlay(false)}
+				className=" absolute top-[20px] right-[20px]   hover:rotate-180 transition-all duration-300 cursor-pointer "
+			>
+				<HiXMark className="text-[40px]" />
+			</div>
+			<div className="w-full 1077:w-[70%] h-full m-auto frc">
+				<div className=" relative overflow-hidden w-[100%] pt-[56.25%]">
+					<iframe
+						frameBorder="0"
+						allowFullScreen
+						//   mozallowfullscreen
+						//   msallowfullscreen="msallowfullscreen"
+						//   oallowfullscreen="oallowfullscreen"
+						//   webkitallowfullscreen="webkitallowfullscreen"
+						width="100%"
+						height="100%"
+						src={`https://www.youtube.com/embed/${item}`}
+						className=" absolute inset-0"
+						title="YouTube video player"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					></iframe>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default VideoPlayer;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,15 @@
-import Hero from 'components/Hero/Hero'
-import React, { FC, useState } from 'react'
+import Hero from "components/Hero/Hero";
+import VideoPlayer from "components/VideoPopup/VideoPlayer";
+import React, { FC, useState } from "react";
 
-const Home:FC = () => {
-    const [play,setPlay] = useState(false)
-  return (
-    <>
-    <Hero play={play} setPlay={setPlay} />
-    </>
-  )
-}
+const Home: FC = () => {
+	const [play, setPlay] = useState(false);
+	return (
+		<main>
+			<Hero play={play} setPlay={setPlay} />
+            {play && <VideoPlayer play={play} setPlay={setPlay}  />}
+		</main>
+	);
+};
 
-export default Home
+export default Home;
